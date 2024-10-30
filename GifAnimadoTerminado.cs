76044a1f5571e28bb7aa645b2fd4206823804281
LUISRODRIGUEZ0408 <rdzluisrdz17@gmail.com>
2024-10-30 12:47:52 -0500

    Add files via upload

---
+++ GifAnimadoTerminado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_del_arduino_y_foco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"C:\Users\Bioxor\Source\Repos\GifSample\GifSample\minion-enamorado");
+            pictureBox1.SizeMode=PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
